50cce8db5fa1eb36f711e430e16f3e92bb71190b
Edogawa-plus <jiuxiaog@outlook.com>
2024-10-23 22:15:31 +0800

    feat # add module: Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,28 @@
-const Login = () => {
-  return <div>Login</div>
-}
+import "./index.scss";
+import { Card, Form, Input, Button } from "antd";
+import logo from '@/assets/login_bg.png'
 
-export default Login
+const Login = () => {
+  return (
+    <div className="login">
+      <Card
+        className="login-card"
+      >
+        <img className="login-logo" src={logo} alt="" />
+        <Form>
+          <Form.Item>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>登录</Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
